5ba1e3a0316c0b1e89011e84e0216a065e5a0433
Tomás González <alu0100913033@ull.edu.es>
2018-11-03 19:12:49 +0000

    Estructura Bundle creada

---
+++ lib/tdd/alu0100913033.rb
@@ -0,0 +1,8 @@
+require "tdd/alu0100913033/version"
+
+module Tdd
+  module Alu0100913033
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/tdd/alu0100913033/version.rb
@@ -0,0 +1,5 @@
+module Tdd
+  module Alu0100913033
+    VERSION = "0.1.0"
+  end
+end
