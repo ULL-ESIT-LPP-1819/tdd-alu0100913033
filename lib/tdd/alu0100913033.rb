7f8c3041cda9ec61156495f1033627ad869f3cd2
Tomás González <alu0100913033@ull.edu.es>
2018-11-03 20:02:33 +0000

    Cambiando nombre de la gema

--- lib/tdd/alu0100913033.rb
+++
@@ -1,8 +0,0 @@
-require "tdd/alu0100913033/version"
-
-module Tdd
-  module Alu0100913033
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- lib/tdd/alu0100913033/version.rb
+++
@@ -1,5 +0,0 @@
-module Tdd
-  module Alu0100913033
-    VERSION = "0.1.0"
-  end
-end
