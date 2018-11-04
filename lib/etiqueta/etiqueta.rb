370a19d08e863a982e56bf2b09d04bed26532868
Tomás González <alu0100913033@ull.edu.es>
2018-11-04 20:14:19 +0000

    Actualización del initialize con parámetro nombre añadido

--- lib/etiqueta/etiqueta.rb
+++ lib/etiqueta/etiqueta.rb
@@ -2,7 +2,8 @@ module Etiqueta
   class Etiqueta
     attr_reader :v_energetico, :grasas, :grasas_sat, :hidratos, :azucares, :proteinas, :sal
 
-    def initialize (v_energetico, grasas, grasas_sat, hidratos, azucares, proteinas, sal)
+    def initialize (nombre, v_energetico, grasas, grasas_sat, hidratos, azucares, proteinas, sal)
+      @nombre = nombre
       @v_energetico = v_energetico
       @grasas = grasas
       @grasas_sat = grasas_sat
--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -2,9 +2,17 @@ require 'spec_helper'
 require 'etiqueta'
 
 RSpec.describe Etiqueta do
-  context "# inicialización de la clase etiqueta"
-  it "Valor nutricional de una manzana" do
-    #v_energetico, grasas, grasas_sat, hidratos, azucares, proteinas, sal
-    apple = Etiqueta::Etiqueta.new(108.16, 0.72, 0.12, 22.80, 20.62, 0.63, 2.40)
+
+  #Creación correcta de una clase
+  context "# Inicialización de la clase etiqueta"
+  it "Se crea una etiqueta \"manzana\" correctamente" do
+    apple = Etiqueta::Etiqueta.new("manzana", 108.16, 0.72, 0.12, 22.80, 20.62, 0.63, 2.40)
   end
+
+  #
+
+
+
+
+
 end
