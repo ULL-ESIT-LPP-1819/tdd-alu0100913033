9f2d47af01b465e67943aaa6dc424a78fc3f5009
Tomás González <alu0100913033@ull.edu.es>
2018-11-06 18:09:33 +0000

    Pruebas correctas sobre los nutrientes

--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe Etiqueta do
    #Creación correcta de una clase
   context "# Inicialización de la clase etiqueta" do
     it "Se crea una etiqueta \"manzana\" correctamente" do
-      apple = Etiqueta::Etiqueta.new("manzana", 108.16, 0.72, 0.12, 0.30, 0.21, 2.78, 0.0, 4.04, 72.82, 289.18, 20.62, 20.62, 0.31, 1.2)
+      apple = Etiqueta::Etiqueta.new("manzana", 108.16, 0.72, 0.12, 0.30, 0.21, 2.78, 0.0, 4.04, 72.82, 289.18, 22.80, 20.62, 0.63, 2.4)
     end
   end # context
 
   # Existencia de los parámetros
   context "# Comprobar parámetros de la clase" do
     before :each  do
-      @apple = Etiqueta::Etiqueta.new("manzana", 108.16, 0.72, 0.12, 0.30, 0.21, 2.78, 0.0, 4.04, 72.82, 289.18, 20.62, 20.62, 0.31, 1.2)
+      @apple = Etiqueta::Etiqueta.new("manzana", 108.16, 0.72, 0.12, 0.30, 0.21, 2.78, 0.0, 4.04, 72.82, 289.18, 22.80, 20.62, 0.63, 2.4)
     end
 
 
@@ -74,23 +74,23 @@ RSpec.describe Etiqueta do
       it "Hidratos de carbono correctamente indicados" do
         expect(@apple.hidratos).not_to be_nil
         expect(@apple.hidratos).to be >= 0
-        expect(@apple.hidratos).to eq(20.62)
+        expect(@apple.hidratos).to eq(22.8)
       end
       it "Azúcares correctamente indicados" do
         expect(@apple.azucares).not_to be_nil
         expect(@apple.azucares).to be >= 0
         expect(@apple.azucares).to eq(20.62)
       end
-      #it "Proteínas correctamente indicadas" do
-      #  expect(@apple.proteinas).not_to be_nil
-      #  expect(@apple.proteinas).to be >= 0
-      #  expect(@apple.proteinas).to eq(0.63)
-      #end
-      #it "Sal correctamente indicada" do
-      #  expect(@apple.sal).not_to be_nil
-      #  expect(@apple.sal).to be >= 0
-      #  expect(@apple.sal).to eq(2.40)
-      #end
+      it "Proteínas correctamente indicadas" do
+        expect(@apple.proteinas).not_to be_nil
+        expect(@apple.proteinas).to be >= 0
+        expect(@apple.proteinas).to eq(0.63)
+      end
+      it "Sal correctamente indicada" do
+        expect(@apple.sal).not_to be_nil
+        expect(@apple.sal).to be >= 0
+        expect(@apple.sal).to eq(2.4)
+      end
     end
 
 
