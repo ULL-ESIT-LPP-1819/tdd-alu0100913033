649487bd678ea5e2e1812fb959d973e2ef98b66a
Tomás González <alu0100913033@ull.edu.es>
2018-12-18 18:29:30 +0000

    Coveralls especificado

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "bundler/setup"
 require "etiqueta/etiqueta"
 require "etiqueta/individuo"
+require ’coveralls’
+Coveralls.wear!
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
